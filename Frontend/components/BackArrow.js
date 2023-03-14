0327c631d29f19bd342e73911859c6e9225ae764
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-14 20:43:10 +0100

    deleted BackArrow component name

--- Frontend/components/BackArrow.js
+++ Frontend/components/BackArrow.js
@@ -10,8 +10,7 @@ export default function BackArrow({text}) {
   return (
     <TouchableOpacity
         onPress={navigation.goBack}
-        className="bg-[#6159E6] rounded-b-full p-2 flex items-center self-center">
-        <Text className="text-xs text-white">{text}</Text>
+        className="bg-[#6159E6] rounded-full p-2 flex items-center self-center">
         <ArrowLeftIcon size={30} color='white'/>
     </TouchableOpacity>
   )
