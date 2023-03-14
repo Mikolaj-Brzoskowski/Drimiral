af1224599a32decba86e602f6edfde3dc4ad602d
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-14 17:20:41 +0100

    moved adding note func to journal screen

--- Frontend/components/AddNote.js
+++ Frontend/components/AddNote.js
@@ -1,26 +1,21 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { PlusIcon } from 'react-native-heroicons/solid'
-import Note from './Note';
 
-export default function AddNote({onAddClick, inputList}) {
-
-  const onAddBtnClick = event => {
-    onAddClick(inputList.concat(<Note key={inputList.length}/>));
-  };
+export default function AddNote({onAddBtnClick}) {
 
   return (
     <View>
         <View className="p-3 pb-0">
             <TouchableOpacity 
-            className="bg-black rounded-full p-2 self-center"
+            className="bg-[#6159E6] rounded-full p-2 self-center"
             onPress={onAddBtnClick}
             >
             <PlusIcon size={30} color='white'/>
             </TouchableOpacity>
         </View>
         <View className="ml-2 mr-2">
-            <Text>Add note</Text>
+            <Text>Add Note</Text>
         </View>
     </View>
   )
