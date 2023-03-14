e11cf7bb7e3e783f7a58264514cda92c7633aba3
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-14 20:44:35 +0100

    added dynamic save/delete buttons render and value handlers

--- Frontend/components/Note.js
+++ Frontend/components/Note.js
@@ -1,8 +1,32 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { CheckIcon, TrashIcon } from 'react-native-heroicons/solid'
 
-export default function Note({title, note, isDeleteButtonActive}) {
+export default function Note({id, title, note, isDeleteButtonActive, onDelConfirmButtonClick, onSaveBtnClick}) {
+
+  const [titleValue, setTitleValue] = useState(title)
+  const [noteValue, setNoteValue] = useState(note)
+  const [valChanged, setValChanges] = useState(false)
+
+  const conditionalRender = () => {
+    if (isDeleteButtonActive) {
+      return (<View className="flex-column mr-2 mt-1 justify-center items-center">
+      <TouchableOpacity className="bg-[#6159E6] rounded-full p-2" onPress={(e) => onDelConfirmButtonClick(id)}>
+        <TrashIcon size={30} color='white'/>
+      </TouchableOpacity>
+      <Text>Confirm Delete</Text>
+      </View>)
+    }
+    else if (valChanged) {
+      return (<View className="flex-column mr-2 mt-1 justify-center items-center">
+      <TouchableOpacity className="bg-[#6159E6] rounded-full p-2" onPress={(e) => {onSaveBtnClick(id, titleValue, noteValue); setValChanges(false)}}>
+        <CheckIcon size={30} color='white'/>
+      </TouchableOpacity>
+      <Text>Save Note</Text>
+      </View>)
+    }
+    else return null
+  }
 
   return (
     <View className="flex-column border-t-2 border-gray-300 p-2 w-screen">
@@ -11,29 +35,21 @@ export default function Note({title, note, isDeleteButtonActive}) {
         keyboardType="default" 
         numberOfLines={1} 
         className="font-bold text-2xl" 
-        defaultValue={title}/>
+        defaultValue={titleValue}
+        onChangeText={setTitleValue}
+        onChange={(e) => setValChanges(true)}
+        />
         <TextInput 
         placeholder="Type note here..." 
         keyboardType="default" 
-        defaultValue={note} 
+        defaultValue={noteValue}
         multiline={true} 
-        className="text-base flex-row w-full bg-gray-100 border-solid border-2 border-[#6159E6] rounded self-center p-1 m-1 mr-3"/>
+        className="text-base flex-row w-full bg-gray-100 border-solid border-2 border-[#6159E6] rounded self-center p-1 m-1 mr-3"
+        onChangeText={setNoteValue}
+        onChange={(e) => setValChanges(true)}
+        />
         <View>
-          {isDeleteButtonActive ?
-          (<View className="flex-column mr-2 mt-1 justify-center items-center">
-          <TouchableOpacity className="bg-[#6159E6] rounded-full p-2">
-            <TrashIcon size={30} color='white'/>
-          </TouchableOpacity>
-          <Text>Confirm Delete</Text>
-          </View>
-          )
-          :
-          <View className="flex-column mr-2 mt-1 justify-center items-center">
-            <TouchableOpacity className="bg-[#6159E6] rounded-full p-2">
-              <CheckIcon size={30} color='white'/>
-            </TouchableOpacity>
-            <Text>Save Note</Text>
-          </View>}
+          {conditionalRender()}
         </View>
     </View>
   )
