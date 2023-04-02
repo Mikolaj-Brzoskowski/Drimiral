89039802541ad78a09f43371756ed5de7fe4bb1b
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-04-02 11:41:08 +0200

    added basics question screen

---
+++ Frontend/components/Question.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Question({question, type}) {
+  return (
+    <Text className="">{question}</Text>
+  )
+}
--- Frontend/screens/HomeScreen.js
+++ Frontend/screens/HomeScreen.js
@@ -1,20 +1,23 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React, {useLayoutEffect} from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
+
+  const navigation = useNavigation()
 
   return (
     <ScrollView className="pt-2 bg-white h-auto p-1">
       <Text className="text-red-500">Home</Text>
-      <View className="flex-column flex-1 justify-center content-between items-center border-solid border-2 border-gray-300 rounded">
-        <Text className="self-center p-2 w-10/12 text-3xl font-bold text-center">Survey</Text>
-        <Image
-        source={{uri: 'https://cdn-icons-png.flaticon.com/512/10075/10075503.png'}}
-        className=" h-60 w-60"/>
-        <Text className="text-base p-1 m-1">Please</Text>
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Survey')}>
+        <View className="flex-column flex-1 justify-center content-between items-center border-solid border-2 border-gray-300 rounded">
+          <Text className="self-center p-2 w-10/12 text-3xl font-bold text-center">Survey</Text>
+          <Image
+          source={{uri: 'https://cdn-icons-png.flaticon.com/512/10075/10075503.png'}}
+          className=" h-60 w-60"/>
+          <Text className="text-base p-1 m-1">Please take a minute to answer questions about your sleep.</Text>
+        </View>
+      </TouchableOpacity>
     </ScrollView>
   )
 }
