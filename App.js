027a4903879eaad2e2c9d9d9dd924fa9b6af9a8d
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-04-16 21:24:53 +0200

    cleanup

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react'
 import Navigation from './Navigation';
-import * as Network from 'expo-network';
 
 const App = props => {
   
